54bec9b564fa75b35baaeeb204e739ebc3a76087
Theekshana Chathuranga <theekshanacnb@gmail.com>
2025-04-25 12:25:02 +0530

    Main

---
+++ src/main/java/ruh/ac/productsystem/ProductSystemApplication.java
@@ -0,0 +1,13 @@
+package ruh.ac.productsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSystemApplication.class, args);
+		System.out.println("Product System Application Started");
+	}
+}
---
+++ src/test/java/ruh/ac/productsystem/ProductSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ruh.ac.productsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
