01b8be371f019f2f2b08a53f78b0b4c18329c17b
Theekshana Chathuranga <theekshanacnb@gmail.com>
2025-04-25 12:41:40 +0530

    Set geters and setters

---
+++ src/main/java/ruh/ac/productsystem/entity/Product.java
@@ -0,0 +1,60 @@
+package ruh.ac.productsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private String made_in;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getMade_in() {
+        return made_in;
+    }
+
+    public void setMade_in(String made_in) {
+        this.made_in = made_in;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+}
